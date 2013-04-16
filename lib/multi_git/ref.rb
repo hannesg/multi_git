e7a87dea2b7a44a27220b2dbf5f44f848c8aabd4
Hannes Georg <hannes.georg@xing.com>
2013-04-16 19:43:05 +0200

    initial ref spec

--- lib/multi_git/ref.rb
+++ lib/multi_git/ref.rb
@@ -10,6 +10,23 @@ module MultiGit
       @name = name
     end
 
+    # @!method reload
+    #   @return [MultiGit::Ref]
+    abstract :reload
+
+    # @!method target
+    #   @return [MultiGit::Ref, MultiGit::Object, nil]
+    abstract :target
+
+    # @!method lock!( mode = :pessimistic )
+    #   @param mode [:pessimistic, :optimistic]
+    #   @return [Boolean]
+    abstract :lock!
+
+    # @!method unlock!
+    #   @return [Boolean]
+    abstract :unlock!
+
   end
 
 end
